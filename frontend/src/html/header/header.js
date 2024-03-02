8e022c18a5f93637e14693262cf3ec0ec337c7c5
Tarhun <TarhunchicKKK@yandex.by>
2024-03-02 12:31:50 +0300

    Full html for header

--- frontend/src/html/header/header.js
+++ frontend/src/html/header/header.js
@@ -16,3 +16,27 @@ menu.addEventListener('click',() => {
         }
     }
 })
+
+
+const theme = document.getElementById('theme')
+let isLightTheme = true
+const themeIcons = document.querySelectorAll('#theme img')
+const accountIcons = document.querySelectorAll('#account img')
+const menuIcons = document.querySelectorAll('#menu img')
+
+theme.addEventListener('click', () => {
+    if (isLightTheme) {
+        // header.style.background ='linear-gradient(90deg, #c773fd,#3d0084)'
+        // header.style.background = 'linear-gradient(0deg, #02d179,#00d175,#002332)'
+        header.style.background ='linear-gradient(90deg, #8aa0c7,#5e6a9d,#30284e)'
+    }
+    else {
+        header.style.background = 'linear-gradient(90deg, #8d90e3,#9ec0ea,#8d90e3)'
+    }
+    for (let i = 0; i < themeIcons.length; i++) {
+        themeIcons[i].classList.toggle('hidden')
+        accountIcons[i].classList.toggle('hidden')
+        menuIcons[i].classList.toggle('hidden')
+    }
+    isLightTheme = !isLightTheme
+})
