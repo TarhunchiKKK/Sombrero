a737bc0c724c0d66532e41e3468f8160dfee647e
Tarhun <TarhunchicKKK@yandex.by>
2024-03-07 18:10:21 +0300

    Create home page with slider. Left to add images

---
+++ frontend/src/utils/getWindowSize.ts
@@ -0,0 +1,12 @@
+export default function getWindowSize(): string {
+    const windowSize = window.innerWidth
+    if (windowSize <= 640) {
+        return 'sm'
+    }
+    else if (640 < windowSize && windowSize <= 1024) {
+        return 'md'
+    }
+    else {
+        return 'lg'
+    }
+}
