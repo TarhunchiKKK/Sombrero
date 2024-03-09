bc0a5d19182970156e9933a5a7e9c7022edf3953
Tarhun <TarhunchicKKK@yandex.by>
2024-03-09 23:38:58 +0300

    Move  AccordeonItem to separate component

---
+++ frontend/src/components/AccordeonItem.tsx
@@ -0,0 +1,38 @@
+import AccodreonItem from '../data/faq'
+
+interface AccordeonItemProps {
+    item: AccodreonItem
+    isOpen: boolean
+    onClick: () => void
+}
+
+export default function AccordeonItem({
+    item,
+    isOpen,
+    onClick,
+}: AccordeonItemProps) {
+    const questionStyle: string = isOpen
+        ? 'cursor-pointer h-14 px-4 py-1 flex justify-between items-center border-[#BEC2C6] border-2 main-gradient'
+        : 'cursor-pointer h-14 px-4 py-1 flex justify-between items-center border-[#BEC2C6] border-2'
+
+    const answerStyle: string = isOpen
+        ? 'bg-gray-200 border-[#BEC2C6] border-2 border-t-0 px-4 py-1 block '
+        : 'bg-gray-200 border-[#BEC2C6] border-2 border-t-0 px-4 py-1 hidden '
+
+    const angleStyle: string = isOpen
+        ? 'rotate-180 fa-solid fa-angle-up'
+        : '-rotate-90 fa-solid fa-angle-up'
+
+    return (
+        <div className='-mt-[2px] first:mt-0'>
+            <div onClick={onClick} className={questionStyle}>
+                <span className='text-3xl'>{item.question}</span>
+                <i className={angleStyle}></i>
+            </div>
+
+            <div className={answerStyle}>
+                <p className='text-xl'>{item.answer}</p>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/FaqPage.tsx
+++ frontend/src/pages/FaqPage.tsx
@@ -1,38 +1,6 @@
 import { useMemo, useState } from 'react'
-import AccodreonItem, { AccordeonItems } from '../data/faq'
-
-interface AccordeonItemProps {
-    item: AccodreonItem
-    isOpen: boolean
-    onClick: () => void
-}
-
-function AccordeonItem({ item, isOpen, onClick }: AccordeonItemProps) {
-    const questionStyle: string = isOpen
-        ? 'cursor-pointer h-14 px-4 py-1 flex justify-between items-center border-[#BEC2C6] border-2 main-gradient'
-        : 'cursor-pointer h-14 px-4 py-1 flex justify-between items-center border-[#BEC2C6] border-2'
-
-    const answerStyle: string = isOpen
-        ? 'bg-gray-200 border-[#BEC2C6] border-2 border-t-0 px-4 py-1 block '
-        : 'bg-gray-200 border-[#BEC2C6] border-2 border-t-0 px-4 py-1 hidden '
-
-    const angleStyle: string = isOpen
-        ? 'rotate-180 fa-solid fa-angle-up'
-        : '-rotate-90 fa-solid fa-angle-up'
-
-    return (
-        <div className='-mt-[2px] first:mt-0'>
-            <div onClick={onClick} className={questionStyle}>
-                <span className='text-3xl'>{item.question}</span>
-                <i className={angleStyle}></i>
-            </div>
-
-            <div className={answerStyle}>
-                <p className='text-xl'>{item.answer}</p>
-            </div>
-        </div>
-    )
-}
+import { AccordeonItems } from '../data/faq'
+import AccordeonItem from '../components/AccordeonItem'
 
 export default function FaqPage() {
     const items = useMemo(() => AccordeonItems, [])
