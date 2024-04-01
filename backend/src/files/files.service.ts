51a1d13a87b6e6ab21e7058ec7e4b67916ed81f0
Tarhun <TarhunchicKKK@yandex.by>
2024-04-01 12:48:00 +0300

    Add auto creating directories

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -1,50 +1,55 @@
 import { Injectable, StreamableFile } from '@nestjs/common';
-import path, { join } from 'path';
 const fs = require('fs');
+const path = require('path');
 
 @Injectable()
 export class FilesService {
-    private readonly accountsDir: string = path.join('static/account/');
-    private readonly advertisementsDir: string = path.join('static/products/');
-    private readonly categoriesDir: string = path.join('static/categories/');
+    private readonly accountsDir: string = path.join('data/account/');
+    private readonly advertisementsDir: string = path.join('data/products/');
+    private readonly categoriesDir: string = path.join('data/categories/');
 
     constructor() {
-        fs.mkdirSync(path.join('static'));
-        fs.mkdirSync(this.accountsDir);
-        fs.mkdirSync(this.advertisementsDir);
-        fs.mkdirSync(this.categoriesDir);
+        if (!fs.existsSync(this.accountsDir)) {
+            fs.mkdirSync(this.accountsDir);
+        }
+        if (!fs.existsSync(this.advertisementsDir)) {
+            fs.mkdirSync(this.advertisementsDir);
+        }
+        if (!fs.existsSync(this.categoriesDir)) {
+            fs.mkdirSync(this.categoriesDir);
+        }
     }
 
     public uploadAccountImage(file: Express.Multer.File): void {
-        const writeStream = fs.createWriteStream(join(this.accountsDir, file.originalname));
+        const writeStream = fs.createWriteStream(path.join(this.accountsDir, file.originalname));
         writeStream.write(file.buffer);
         writeStream.close();
     }
 
     public downloadAccountImage(fileName: string): StreamableFile {
-        const readStream = fs.createReadStream(join(this.accountsDir, fileName));
+        const readStream = fs.createReadStream(path.join(this.accountsDir, fileName));
         return new StreamableFile(readStream);
     }
 
     public uploadAdvertisementImage(file: Express.Multer.File): void {
-        const writeStream = fs.createWriteStream(join(this.advertisementsDir, file.originalname));
+        const writeStream = fs.createWriteStream(path.join(this.advertisementsDir, file.originalname));
         writeStream.write(file.buffer);
         writeStream.close();
     }
 
     public downloadAdvertisementImage(fileName: string): StreamableFile {
-        const readStream = fs.createReadStream(join(this.advertisementsDir, fileName));
+        const readStream = fs.createReadStream(path.join(this.advertisementsDir, fileName));
         return new StreamableFile(readStream);
     }
 
     public uploadCategoryImage(file: Express.Multer.File): void {
-        const writeStream = fs.createWriteStream(join(this.categoriesDir, file.originalname));
+        const writeStream = fs.createWriteStream(path.join(this.categoriesDir, file.originalname));
         writeStream.write(file.buffer);
         writeStream.close();
     }
 
     public downloadCategoryImage(fileName: string): StreamableFile {
-        const readStream = fs.createReadStream(join(this.categoriesDir, fileName));
+        const readStream = fs.createReadStream(path.join(this.categoriesDir, fileName));
         return new StreamableFile(readStream);
     }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const fs = require('fs');
+
+function createFilesStorage() {
+    if (!fs.existsSync(process.env.FILES_STORAGE)) {
+        fs.mkdirSync(process.env.FILES_STORAGE);
+    }
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    createFilesStorage();
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
 }
 bootstrap();
