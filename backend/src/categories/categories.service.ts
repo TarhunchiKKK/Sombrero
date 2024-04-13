e4d05accc4b82d5f3ce564d65c7d16c1a91b6111
Tarhun <TarhunchicKKK@yandex.by>
2024-04-13 11:25:09 +0300

    Normalize categories module

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -3,45 +3,7 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
-
-const categories: CreateCategoryDto[] = [
-    {
-        title: 'Auto',
-        advertisements: [],
-    },
-    {
-        title: 'For work',
-        advertisements: [],
-    },
-    {
-        title: 'Clothes',
-        advertisements: [],
-    },
-    {
-        title: 'Hobbies and relax',
-        advertisements: [],
-    },
-    {
-        title: 'Animals',
-        advertisements: [],
-    },
-    {
-        title: 'For kids',
-        advertisements: [],
-    },
-    {
-        title: 'Electronics',
-        advertisements: [],
-    },
-    {
-        title: 'Beauti and health',
-        advertisements: [],
-    },
-    {
-        title: 'Spare parts',
-        advertisements: [],
-    },
-];
+import { categories } from './data/categories';
 
 @Injectable()
 export class CategoriesService {
