49358f4bcfabdf184476895a0e149404d6eb4df2
Tarhun <TarhunchicKKK@yandex.by>
2024-04-13 11:49:49 +0300

    Add auto loading faqs

--- backend/src/faq/faq.service.ts
+++ backend/src/faq/faq.service.ts
@@ -2,15 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { Faq } from './entities/faq.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { faqs } from './data/faqs';
 
 @Injectable()
 export class FaqService {
-  constructor(
-    @InjectRepository(Faq)
-    private readonly faqRepository: Repository<Faq>
-  ) {}
+    constructor(
+        @InjectRepository(Faq)
+        private readonly faqsRepository: Repository<Faq>,
+    ) {
+        Promise.resolve(this.faqsRepository.clear()).then(() => this.faqsRepository.save(faqs));
+    }
 
-  findAll() {
-    return []
-  }
+    public async findAll(): Promise<Faq[]> {
+        return await this.faqsRepository.find();
+    }
+
+    public async findOne(id: number): Promise<Faq> {
+        return await this.faqsRepository.findOne({
+            where: {
+                id: id,
+            },
+        });
+    }
 }
