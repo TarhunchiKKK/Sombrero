df231b857c6349562c2cf90c0fa49046fca85415
Tarhun <TarhunchicKKK@yandex.by>
2024-04-14 20:40:59 +0300

    Add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const fs = require('fs');
+const cors = require('cors');
 
 function createFilesStorage() {
     if (!fs.existsSync(process.env.FILES_STORAGE)) {
@@ -12,6 +13,7 @@ function createFilesStorage() {
 async function bootstrap() {
     createFilesStorage();
     const app = await NestFactory.create(AppModule);
+    app.use(cors());
     await app.listen(3000);
 }
 bootstrap();
