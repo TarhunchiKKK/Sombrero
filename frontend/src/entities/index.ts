ef0949134029b58e1d79387ec8ba88141853bdc6
Tarhun <TarhunchicKKK@yandex.by>
2024-04-15 14:53:56 +0300

    Some clean

--- frontend/src/entities/index.ts
+++
@@ -1,3 +0,0 @@
-export type { ICategory } from './category/models/ICategory.ts';
-export type { IAdvertisement } from './advertisement/models/IAdvertisement.ts';
-export type { IUser } from './user/models/IUser.ts';
