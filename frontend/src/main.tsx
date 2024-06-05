ee94da3364fb823a346c8406e3d8ec12eedf4995
konstantin <konstabarilo12@gmail.com>
2024-06-05 17:26:54 +0300

    Move loader to the components folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/styles/index.css';
-import { router, store } from './app/index.ts';
+import { store } from './app/index.ts';
 import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
 import { AuthModalLayout } from './widgets/authModal/context/AuthModalContext.tsx';
 import { App } from './App.tsx';
 
